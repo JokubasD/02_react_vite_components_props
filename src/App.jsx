25f7f2c36925c55b858ae6986fcabe6ea1471e98
JokubasD <jokubasdimsa@gmail.com>
2022-11-22 10:20:15 +0200

    our services

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,81 @@
 import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0);
-
+  function OurServicesTitle(props) {
+    return <h2 className='sectionTitle'>{props.title}</h2>;
+  }
+  function Icon(props) {
+    console.log('props.iconName ===', props.iconName);
+    return (
+      <i
+        className={`fa card__icon fa-3x ${props.iconName}`}
+        aria-hidden='true'
+      ></i>
+    );
+  }
+  function Grid() {
+    return (
+      <div className='grid'>
+        <ServiceCard
+          title='Business Consulting'
+          icon='fa-address-book'
+          link='/about.html'
+        />
+        <ServiceCard
+          title='Market Analysis'
+          icon='fa-grav'
+          link='/about.html'
+        />
+        <ServiceCard
+          title='User Monitoring'
+          icon='fa-id-card-o'
+          link='/about.html'
+        />
+        <ServiceCard
+          title='Business Consulting'
+          icon='fa-address-book'
+          link='/about.html'
+        />
+        <ServiceCard
+          title='Market Analysis'
+          icon='fa-grav'
+          link='/about.html'
+        />
+        <ServiceCard
+          title='User Monitoring'
+          icon='fa-id-card-o'
+          link='/about.html'
+        />
+      </div>
+    );
+  }
+  function ServiceCard(props) {
+    console.log('props.icon ===', props.icon);
+    return (
+      <div className='card'>
+        <Icon iconName={props.icon} />
+        <h3 className='card__title'>{props.title}</h3>
+        <p className='card__text'>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis
+          quis molestiae vitae eligendi at.
+        </p>
+        <a href={props.link} className='card__link'>
+          Learn More
+        </a>
+      </div>
+    );
+  }
+  function OurServices() {
+    return (
+      <div className='servicesSection'>
+        <OurServicesTitle title='Our Services' />
+        <Grid></Grid>
+      </div>
+    );
+  }
   return (
-    <div className="App container">
-      <h1>Hello vite React !!!</h1>
+    <div className='App container'>
+      <OurServices />
     </div>
   );
 }
